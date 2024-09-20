5a81c3307101be5c000eaafdba86632035e80c31
sejal-sharma08 <sejalshrm08@gmail.com>
2024-09-21 01:29:41 +0530

    input-output stream added

--- Server.java
+++ Server.java
@@ -1,8 +1,13 @@
 import java.net.*; 
+import java.io.*;
 
 class Server {
     ServerSocket server;
     Socket socket;
+
+    BufferedReader br;
+    PrintWriter out;
+
     public Server() {
         try {
             server = new ServerSocket(7777);
@@ -10,6 +15,12 @@ class Server {
             System.out.println("waiting...");
             socket = server.accept();
 
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+
+            startReading();
+            startWriting();
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
