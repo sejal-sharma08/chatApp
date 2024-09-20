62c329c6245b7e0793536788f8c930211d8f9b0e
sejal-sharma08 <sejalshrm08@gmail.com>
2024-09-21 01:26:17 +0530

    constructor initialized

--- Server.java
+++ Server.java
@@ -1,4 +1,19 @@
+import java.net.*; 
+
 class Server {
+    ServerSocket server;
+    Socket socket;
+    public Server() {
+        try {
+            server = new ServerSocket(7777);
+            System.out.println("System is ready to accept connection");
+            System.out.println("waiting...");
+            socket = server.accept();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         System.out.println("this is server...going to start server");
         new Server();
