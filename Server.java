146e0a0dcf820a34a08af4f26eb758a43eddd3d7
sejal-sharma08 <sejalshrm08@gmail.com>
2024-09-21 01:32:23 +0530

    threads created

--- Server.java
+++ Server.java
@@ -20,10 +20,24 @@ class Server {
 
             startReading();
             startWriting();
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void startReading() {
+        // this thread will keep giving us data after reading
+        Runnable r1 = ()->{
+         
+        };
+    }
+
+    public void startWriting() {
+        // this thread will take data from the user and then send it back to the client
+        Runnable r2 = ()->{
+
+        };
     }
     public static void main(String[] args) {
         System.out.println("this is server...going to start server");
