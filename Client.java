d3267c256f4aa6137011a5d46938a3e92e107a83
sejal-sharma08 <sejalshrm08@gmail.com>
2024-09-21 17:24:51 +0530

    Constructor initialized

--- Client.java
+++ Client.java
@@ -1,4 +1,18 @@
+import java.net.*;
+
 public class Client {
+
+    Socket socket;
+
+    public Client() {
+        try {
+            System.out.println("Sending request to server");
+            socket = new Socket("127.0.0.1", 7777);
+            System.out.println("Connection done");
+        } catch (Exception e) {
+            
+        }
+    }
     public static void main(String[] args) {
         System.out.println("this is client..");
     } 
